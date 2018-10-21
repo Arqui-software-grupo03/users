e57ad8cdf73487b9dcaf9c44b476b0735aca82ec
cjsalame <cjsalame@uc.cl>
2018-10-21 18:54:42 -0300

    can login by post request

--- src/urls.py
+++ src/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 
-from mysite.views import RegistrationAPIView
+from mysite.views import LoginAPIView, RegistrationAPIView
 
 urlpatterns = [
     url(r'^users/?$', RegistrationAPIView.as_view()),
+    url(r'^users/login/?$', LoginAPIView.as_view()),
 ]
