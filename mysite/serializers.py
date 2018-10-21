f980cfb0f4898589953011908480bf6229481492
cjsalame <cjsalame@uc.cl>
2018-10-21 18:01:02 -0300

    users can be registered by post request

--- mysite/serializers.py
+++ mysite/serializers.py
@@ -1,14 +1,23 @@
-# from django.contrib.auth.models import User, Group
-# from rest_framework import serializers
-#
-#
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('url', 'email', 'groups')
-#
-#
-# class GroupSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = Group
-#         fields = ('url', 'name')
+from rest_framework import serializers
+from .models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """Serializers registration requests and creates a new user."""
+
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    # The client should not be able to send a token along with a registration
+    # request. Making `token` read-only handles that for us.
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'token']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
